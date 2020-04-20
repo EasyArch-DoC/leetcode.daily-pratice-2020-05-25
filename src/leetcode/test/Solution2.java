d6ac370666b7fa08d5e00e43013d8ce593715850
dc <3101348575@qq.com>
2020-04-20 19:40:15 +0800

    leetcode2

---
+++ src/leetcode/test/Solution2.java
@@ -0,0 +1,83 @@
+package leetcode.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+        示例 1:
+
+        输入: "abcabcbb"
+        输出: 3
+        解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+        示例 2:
+
+        输入: "bbbbb"
+        输出: 1
+        解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+        示例 3:
+
+        输入: "pwwkew"
+        输出: 3
+        解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+             请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+        来源：力扣（LeetCode）
+        链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。*/
+public class Solution2 {
+    //方法1 很耗时间，适合字符串量小的
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j <= n; j++)
+                if (allUnique(s, i, j)) ans = Math.max(ans, j - i);
+        return ans;
+    }
+
+    public boolean allUnique(String s, int start, int end) {
+        Set<Character> set = new HashSet<>();
+        for (int i = start; i < end; i++) {
+            Character ch = s.charAt(i);
+            if (set.contains(ch)) return false;
+            set.add(ch);
+        }
+        return true;
+    }
+
+//    方法2，滑动窗口法（重点）
+//    public int lengthOfLongestSubstring(String s) {
+//        int n = s.length(), ans = 0;
+//        Map<Character, Integer> map = new HashMap<>(); // current index of character
+//        // try to extend the range [i, j]
+//        for (int j = 0, i = 0; j < n; j++) {
+//            if (map.containsKey(s.charAt(j))) {
+//                i = Math.max(map.get(s.charAt(j)), i);
+//            }
+//            ans = Math.max(ans, j - i + 1);
+//            map.put(s.charAt(j), j + 1);
+//        }
+//        return ans;
+//    }
+//
+
+    /*滑动窗口是数组/字符串问题中常用的抽象概念。
+    窗口通常是在数组/字符串中由开始和结束索引定义的一系列元素的集合，即 [i, j)[i,j)（左闭，右开）。
+    而滑动窗口是可以将两个边界向某一方向“滑动”的窗口。
+    例如，我们将 [i, j)[i,j) 向右滑动 11 个元素，则它将变为 [i+1, j+1)[i+1,j+1)（左闭，右开）。
+*/
+
+//    方法3，假设字符集
+//    public int lengthOfLongestSubstring(String s) {
+//        int n = s.length(), ans = 0;
+//        int[] index = new int[128]; // current index of character
+//        // try to extend the range [i, j]
+//        for (int j = 0, i = 0; j < n; j++) {
+//            i = Math.max(index[s.charAt(j)], i);
+//            ans = Math.max(ans, j - i + 1);
+//            index[s.charAt(j)] = j + 1;
+//        }
+//        return ans;
+//    }
+}
