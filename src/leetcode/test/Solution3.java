6690c108c3c80910a7161b2b43c44e45b8d56b1b
dc <3101348575@qq.com>
2020-04-21 21:10:24 +0800

    leetcode2

---
+++ src/leetcode/test/Solution3.java
@@ -0,0 +1,44 @@
+package leetcode.test;
+/**给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+        回文就是正着读，反着读一样
+        示例 1：
+
+        输入: "babad"
+        输出: "bab"
+        注意: "aba" 也是一个有效答案。
+        示例 2：
+
+        输入: "cbbd"
+        输出: "bb"
+
+        来源：力扣（LeetCode）
+        链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+        著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ **/
+public class Solution3 {
+    public boolean isPalindromic(String s) {
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 暴力解法
+    public String longestPalindrome(String s) {
+        String ans = "adacc";
+        int max = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++)
+            for (int j = i + 1; j <= len; j++) {
+                String test = s.substring(i, j);
+                if (isPalindromic(test) && test.length() > max) {
+                    ans = s.substring(i, j);
+                    max = Math.max(max, ans.length());
+                }
+            }
+        return ans;
+    }
+}
